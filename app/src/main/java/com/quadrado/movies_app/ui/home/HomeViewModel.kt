eef5692cca4dd98ea8a7e4cd008a65acbffd1d88
Brunno Quadrado <bbertoninuti@gmail.com>
2025-05-20 12:27:49 -0300

    added icons and configuration for bottom bar

--- app/src/main/java/com/quadrado/movies_app/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/quadrado/movies_app/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "testeeeeeeeeeee"
     }
     val text: LiveData<String> = _text
 }
