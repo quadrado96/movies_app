f687ecc1d3e6648fc479940aba024245525a94d5
Brunno Quadrado <bbertoninuti@gmail.com>
2025-06-03 11:14:44 -0300

    info activity on settings fragment

--- app/src/main/java/com/quadrado/movies_app/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/quadrado/movies_app/ui/settings/SettingsFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.quadrado.movies_app.FavoriteMoviesActivity
+import com.quadrado.movies_app.InfoActivity
 import com.quadrado.movies_app.databinding.FragmentSettingsBinding
 import com.quadrado.movies_app.database.Database
 import kotlinx.coroutines.CoroutineScope
@@ -49,7 +50,8 @@ class SettingsFragment : Fragment() {
         }
 
         binding.btnSobre.setOnClickListener {
-            Toast.makeText(requireContext(), "oiii", Toast.LENGTH_SHORT).show()
+            val intent = Intent(requireContext(), InfoActivity::class.java)
+            startActivity(intent)
         }
     }
 
