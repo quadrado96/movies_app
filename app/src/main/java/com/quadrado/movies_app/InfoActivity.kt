6adc2119eba536d8c8a563eac7db0a89863e2c75
Brunno Quadrado <bbertoninuti@gmail.com>
2025-06-05 12:54:47 -0300

    final commit :)

--- app/src/main/java/com/quadrado/movies_app/InfoActivity.kt
+++ app/src/main/java/com/quadrado/movies_app/InfoActivity.kt
@@ -1,8 +1,11 @@
 package com.quadrado.movies_app
 
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.HtmlCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -16,6 +19,17 @@ class InfoActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        supportActionBar?.hide()
+
+        val overviewTextView: TextView = findViewById(R.id.text_geral)
+        overviewTextView.text = HtmlCompat.fromHtml(
+            getString(R.string.about_project_overview),
+            HtmlCompat.FROM_HTML_MODE_LEGACY
+        )
+        overviewTextView.movementMethod = LinkMovementMethod.getInstance()
+        overviewTextView.linksClickable = true
+
     }
 
 
